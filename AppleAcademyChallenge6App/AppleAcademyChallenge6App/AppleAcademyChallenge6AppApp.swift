f1b8d04c48384132e361e41926d6e81bb336df53
AppleDeveloper <euijjang97@gmail.com>
2025-10-20 15:34:47 +0900

    🎉 프로젝트 파일 생성
    1. 프로젝트 파일 생성

---
+++ AppleAcademyChallenge6App/AppleAcademyChallenge6App/AppleAcademyChallenge6AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAcademyChallenge6AppApp.swift
+//  AppleAcademyChallenge6App
+//
+//  Created by Apple Coding machine on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleAcademyChallenge6AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleAcademyChallenge6App/AppleAcademyChallenge6App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleAcademyChallenge6App
+//
+//  Created by Apple Coding machine on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
