7a2886e8aabd977f11dcf8001b2fe53244f2dded
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-10-30 15:13:40 +0900

    ✨ [Feat] 로그인 Enum 작성

---
+++ Findew/Findew/Common/Enum/AlertEnum.swift
@@ -0,0 +1,11 @@
+//
+//  AlertEnum.swift
+//  Findew
+//
+//  Created by 진아현 on 10/30/25.
+//
+
+import Foundation
+
+enum AlertEnum {
+}
---
+++ Findew/Findew/Common/Enum/Login/LoginFieldType.swift
@@ -0,0 +1,65 @@
+//
+//  LoginFieldType.swift
+//  Findew
+//
+//  Created by 진아현 on 10/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum LoginFieldType {
+    case id
+    case password(onOff: Bool)
+    
+    var placeholder: String {
+        switch self {
+        case .id:
+            return "아이디를 입력해주세요"
+        case .password:
+            return "비밀번호를 입력해주세요"
+        }
+    }
+    
+    var loginFieldStrokeColor: Color {
+        return .gray02
+    }
+    
+    var loginFieldStrokeWidth: CGFloat {
+        return 2
+    }
+    
+    var loginFieldFont: Font {
+        return Font.b3
+    }
+    
+    var loginFieldFontColor: Color {
+        return .black
+    }
+    
+    var placeholderColor: Color {
+        return .gray03
+    }
+    
+    var eyeBtnImage: Image? {
+        switch self {
+        case .id:
+            return nil
+        case .password(let onOff):
+            if onOff {
+                return Image(systemName: "eye.fill")
+            } else {
+                return Image(systemName: "eye.slash")
+            }
+        }
+    }
+    
+    var keyboardStyle: UIKeyboardType {
+        switch self {
+        case .id:
+            return .emailAddress
+        case .password(let onOff):
+            return .default
+        }
+    }
+}
