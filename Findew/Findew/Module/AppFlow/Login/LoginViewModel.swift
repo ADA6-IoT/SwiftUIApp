dac4be27c5bdf5ace43828055e70bf5864580b66
AppleDeveloper <ikejhc159@gmail.com>
2025-11-07 03:10:47 +0900

    ♻️ [Refac] AppFlow 및 DI, MVVM 아키텍처 리팩토링

--- Findew/Findew/Module/AppFlow/Login/LoginViewModel.swift
+++ Findew/Findew/Module/AppFlow/Login/LoginViewModel.swift
@@ -6,17 +6,67 @@
 //
 
 import Foundation
+import Combine
 
 @Observable
 class LoginViewModel {
     // MARK: - StateProperty
+    /// 비밀번호 표시
     var showPassword: Bool = false
+    /// 로그인 시 로딩 중
+    var isLoading: Bool = false
+    /// 로그인 성공 및 실패
+    var isLoginFailure: Bool = false
     
     // MARK: - StoreProperty
+    /// 아이디 값
     var id: String = ""
+    /// 비밀번호 값
     var password: String = ""
+    /// 키보드 타입 변경
     var keyboardType: LoginFieldEnum = .id
+    /// 로그인 가능 표시
     var isLoginEnabled: Bool {
         !self.id.isEmpty && !self.password.isEmpty
+    }
+    var alertPrompt: AlertPrompt?
+    
+    // MARK: - Dependency
+    private let container: DIContainer
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    // MARK: - Init
+    init(container: DIContainer) {
+        self.container = container
+    }
+    
+    // MARK: - Method
+    /// 로그인 API 호출
+    /// - Returns: 로그인 성공 실패 여부
+    public func loginAction() -> Result<Bool, Error> {
+        // TODO: - 로그인 Action 작성
+        return .success(true)
+    }
+    
+    /// 로그인 실패 시 처리 함수
+    private func loginFailure() {
+        self.id = ""
+        self.password = ""
+    }
+    
+    /// 로그인 실패 시 Alert 등장
+    public func loginFailureAlert() {
+        alertPrompt = .init(
+            id: .init(),
+            title: "로그인 실패",
+            message: "아이디 및 비밀번호를 다시 입력해주세요",
+            positiveBtnTitle: "확인",
+            positiveBtnAction: { [weak self] in
+                guard let self else { return }
+                self.alertPrompt = nil
+                self.loginFailure()
+            },
+            isPositiveBtnDestructive: false
+        )
     }
 }
---
+++ Findew/Findew/Resources/Modifier/LoadingOverlay.swift
@@ -0,0 +1,63 @@
+//
+//  LoadingOverlay.swift
+//  Findew
+//
+//  Created by Apple Coding machine on 11/7/25.
+//
+
+import SwiftUI
+
+struct LoadingOverlay: ViewModifier {
+    
+    // MARK: - Property
+    let isLoading: Bool
+    let loadingTextType: LoadingTextType
+    
+    // MARK: - Constants
+    fileprivate enum LoadingOverlayConstants {
+        static let opacity: Double = 0.7
+        static let lineSpacing: CGFloat = 2.5
+    }
+    
+    // MARK: - Enum
+    enum LoadingTextType: String {
+        case defaulLoading = "잠시만 기다려주세요"
+        case loginLoading = "로그인 중입니다. \n잠시만 기다려주세요"
+    }
+    
+    // MARK: - Init
+    init(isLoading: Bool, loadingTextType: LoadingTextType) {
+        self.isLoading = isLoading
+        self.loadingTextType = loadingTextType
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(content: {
+                if isLoading {
+                    ZStack {
+                        Color.black.opacity(LoadingOverlayConstants.opacity)
+                            .ignoresSafeArea()
+                            .glassEffectTransition(.matchedGeometry)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        
+                        ProgressView(label: {
+                            Text(loadingTextType.rawValue)
+                                .multilineTextAlignment(.center)
+                                .lineSpacing(LoadingOverlayConstants.lineSpacing)
+                                .font(.b3)
+                                .foregroundStyle(Color.white)
+                        })
+                        .tint(Color.blue04)
+                        .controlSize(.large)
+                    }
+                }
+            })
+    }
+}
+
+extension View {
+    func loadingOverlay(_ isLoading: Bool, loadingTextType: LoadingOverlay.LoadingTextType) -> some View {
+        self.modifier(LoadingOverlay(isLoading: isLoading, loadingTextType: loadingTextType))
+    }
+}
